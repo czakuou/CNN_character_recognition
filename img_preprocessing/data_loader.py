8b101ee8933e47e268790804b3af1ffb23f41b26
czakuou <emil.krzakuou@gmail.com>
2021-04-17 17:43:36 +0200

    data loader add plotting function

--- img_preprocessing/data_loader.py
+++ img_preprocessing/data_loader.py
@@ -2,16 +2,24 @@ import cv2
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import traceback
 
 labels = ["aqua", "megumin"]
 class DataLoader:
-    @staticmethod
-    def get_data(data_dir):
+    '''
+    Load and label images from folders
+    Parameters:
+    data_dor: path to folder with dirs with data
+    '''
+    def __init__(self, data_dir):
+        self.data_dir = data_dir
         # initialize list for data
-        data = []
+        self.data = []
+        
+    def get_data(self):
         # loop through the labels
         for label in labels:
-            path = os.path.join(data_dir, label)
+            path = os.path.join(self.data_dir, label)
             # give index to the label name
             class_num = labels.index(label)
             # loop through the all images
@@ -19,17 +27,20 @@ class DataLoader:
                 try:
                     # read and label images
                     img_arr = cv2.imread(os.path.join(path, img))[...,::-1]
-                    data.append([img_arr, class_num])
-                except Exception as e:
-                    print(e)
+                    self.data.append([img_arr, class_num])
+                except Exception:
+                    print(traceback.format_exc())
         # return numpy array with labeled images
-        return np.array(data, dtype=object)
+        return np.asarray(self.data, dtype=object)
     
+    def plot(self, img_num):
+        # plot labeled images
+        # img_num: number of an image
+        plt.figure(figsize = (5,5))
+        plt.imshow(self.data[img_num][0])
+        plt.title(labels[self.data[img_num][1]])
+        plt.show()
 
-data = ImageLabel()
-data = data.get_data("./cropped/")
-
-plt.figure(figsize = (5,5))
-plt.imshow(data[30][0])
-plt.title(labels[data[30][1]])
-plt.show()
+if __name__ == "__main__":
+    main()
+    
